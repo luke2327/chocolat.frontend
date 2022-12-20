e2104f10dfb88b538621c6106294b9fb4be64676
luke2327 <qwepoi8526@gmail.com>
2022-12-20 22:32:00 +0900

    add: general pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Index = () => {
+  return (
+    <Main
+      meta={
+        <Meta
+          title="Next.js Boilerplate Presentation"
+          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+        />
+      }
+    >
+      <div className="px-4">Test</div>
+    </Main>
+  );
+};
+
+export default Index;
