30053651f05eb328974097ca1f353aee4ba81369
luke2327 <qwepoi8526@gmail.com>
2022-12-21 00:25:34 +0900

    chore: modify index pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ import { Main } from '@/templates/Main';
 const Index = () => {
   return (
     <Main meta={<Meta title="ショコラ" description="" />}>
+      <p className="m-0 text-sm">
+        ショコラは日本と韓国を繋げてくれるアプリです
+      </p>
       <SelectMenu />
     </Main>
   );
