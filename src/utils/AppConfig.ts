cac0b99c395f45dc658cf5f0ddd1fbb9647b07ee
luke2327 <qwepoi8526@gmail.com>
2022-12-20 22:16:32 +0900

    add: init 4

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'ショコラ',
+  title: 'ショコラ',
+  description: 'ショコラは日本と韓国の関連なサイトです',
+  locale: 'ja',
+};
