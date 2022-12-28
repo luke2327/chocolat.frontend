0932df974658d36ee39f2d97f743ed59bc738309
luke2327 <qwepoi8526@gmail.com>
2022-12-28 22:39:38 +0900

    feat(add): constants variables

---
+++ src/constants/temp.ts
@@ -0,0 +1,2 @@
+export const letterFrame =
+  'https://static.vecteezy.com/system/resources/previews/003/758/254/original/black-and-white-thanksgiving-card-im-thankful-for-line-vertical-letter-template-with-cute-turkey-basket-with-apples-fruit-harvest-autumn-outline-holiday-frame-design-for-kids-vector.jpg';
