be943c4e585827bf50e915477f51f1d250881ba5
luke2327 <qwepoi8526@gmail.com>
2023-01-05 14:55:35 +0900

    feat: add i18n init

---
+++ src/i18n/languages/ko.ts
@@ -0,0 +1,19 @@
+import type { Language } from './ja';
+
+const ko: Language = {
+  title: '쇼콜라',
+  description: '소중한 상대에게 편지를 써봐요:)',
+  language: '언어',
+  japanese: '日本語',
+  korean: '한국어',
+
+  'letter.keyword1': '생일',
+  'letter.keyword2': '크리스마스',
+  'letter.keyword3': '발렌타인데이',
+  'letter.keyword4': '화이트데이',
+  'letter.keyword5': '기념일',
+
+  'common.close': '닫기',
+};
+
+export default ko;
