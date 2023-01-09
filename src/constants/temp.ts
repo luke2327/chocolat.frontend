3fa99d4217b5634bc02943157d811394c24bd3cd
luke2327 <qwepoi8526@gmail.com>
2023-01-09 21:26:36 +0900

    feat: set new constants

--- src/constants/temp.ts
+++ src/constants/temp.ts
@@ -1,3 +1,204 @@
 export const letterFrame = {
   123: 'https://static.vecteezy.com/system/resources/previews/003/758/254/original/black-and-white-thanksgiving-card-im-thankful-for-line-vertical-letter-template-with-cute-turkey-basket-with-apples-fruit-harvest-autumn-outline-holiday-frame-design-for-kids-vector.jpg',
 } as const;
+
+export const letterFrameList = [
+  {
+    imgUrl:
+      'https://static.vecteezy.com/system/resources/previews/003/758/254/original/black-and-white-thanksgiving-card-im-thankful-for-line-vertical-letter-template-with-cute-turkey-basket-with-apples-fruit-harvest-autumn-outline-holiday-frame-design-for-kids-vector.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://static.vecteezy.com/system/resources/previews/003/758/254/original/black-and-white-thanksgiving-card-im-thankful-for-line-vertical-letter-template-with-cute-turkey-basket-with-apples-fruit-harvest-autumn-outline-holiday-frame-design-for-kids-vector.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://static.vecteezy.com/system/resources/previews/003/758/254/original/black-and-white-thanksgiving-card-im-thankful-for-line-vertical-letter-template-with-cute-turkey-basket-with-apples-fruit-harvest-autumn-outline-holiday-frame-design-for-kids-vector.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://www.seekpng.com/png/detail/287-2879419_frame-christmas-greeting-card-free-letter-from-father.png',
+    type: 'christmas',
+  },
+  {
+    imgUrl:
+      'https://www.seekpng.com/png/detail/287-2879419_frame-christmas-greeting-card-free-letter-from-father.png',
+    type: 'christmas',
+  },
+  {
+    imgUrl:
+      'https://www.seekpng.com/png/detail/287-2879419_frame-christmas-greeting-card-free-letter-from-father.png',
+    type: 'christmas',
+  },
+  {
+    imgUrl:
+      'https://www.seekpng.com/png/detail/287-2879419_frame-christmas-greeting-card-free-letter-from-father.png',
+    type: 'christmas',
+  },
+  {
+    imgUrl:
+      'https://www.seekpng.com/png/detail/287-2879419_frame-christmas-greeting-card-free-letter-from-father.png',
+    type: 'christmas',
+  },
+  {
+    imgUrl:
+      'https://thumbs.dreamstime.com/b/valentine-frame-design-standard-format-easing-printing-uses-pink-red-shades-features-love-letter-100673691.jpg',
+    type: 'valentine',
+  },
+  {
+    imgUrl:
+      'https://thumbs.dreamstime.com/b/valentine-frame-design-standard-format-easing-printing-uses-pink-red-shades-features-love-letter-100673691.jpg',
+    type: 'valentine',
+  },
+  {
+    imgUrl:
+      'https://thumbs.dreamstime.com/b/valentine-frame-design-standard-format-easing-printing-uses-pink-red-shades-features-love-letter-100673691.jpg',
+    type: 'valentine',
+  },
+  {
+    imgUrl: 'https://en.pimg.jp/075/122/443/1/75122443.jpg',
+    type: 'white',
+  },
+  {
+    imgUrl: 'https://en.pimg.jp/075/122/443/1/75122443.jpg',
+    type: 'white',
+  },
+  {
+    imgUrl: 'https://en.pimg.jp/075/122/443/1/75122443.jpg',
+    type: 'white',
+  },
+  {
+    imgUrl: 'https://en.pimg.jp/075/122/443/1/75122443.jpg',
+    type: 'white',
+  },
+  {
+    imgUrl: 'https://en.pimg.jp/075/122/443/1/75122443.jpg',
+    type: 'white',
+  },
+  {
+    imgUrl: 'https://en.pimg.jp/075/122/443/1/75122443.jpg',
+    type: 'white',
+  },
+  {
+    imgUrl: 'https://en.pimg.jp/075/122/443/1/75122443.jpg',
+    type: 'white',
+  },
+  {
+    imgUrl:
+      'https://i.pinimg.com/736x/8f/10/b2/8f10b2e9a9a97aa2f4c7f7029b763940--free-printable-stationery-printable-paper.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://i.pinimg.com/736x/8f/10/b2/8f10b2e9a9a97aa2f4c7f7029b763940--free-printable-stationery-printable-paper.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://i.pinimg.com/736x/8f/10/b2/8f10b2e9a9a97aa2f4c7f7029b763940--free-printable-stationery-printable-paper.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://i.pinimg.com/736x/8f/10/b2/8f10b2e9a9a97aa2f4c7f7029b763940--free-printable-stationery-printable-paper.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://i.pinimg.com/736x/8f/10/b2/8f10b2e9a9a97aa2f4c7f7029b763940--free-printable-stationery-printable-paper.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://i.pinimg.com/736x/8f/10/b2/8f10b2e9a9a97aa2f4c7f7029b763940--free-printable-stationery-printable-paper.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://i.pinimg.com/736x/8f/10/b2/8f10b2e9a9a97aa2f4c7f7029b763940--free-printable-stationery-printable-paper.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://i.pinimg.com/736x/8f/10/b2/8f10b2e9a9a97aa2f4c7f7029b763940--free-printable-stationery-printable-paper.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://i.pinimg.com/736x/8f/10/b2/8f10b2e9a9a97aa2f4c7f7029b763940--free-printable-stationery-printable-paper.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://static.vecteezy.com/system/resources/previews/003/758/254/original/black-and-white-thanksgiving-card-im-thankful-for-line-vertical-letter-template-with-cute-turkey-basket-with-apples-fruit-harvest-autumn-outline-holiday-frame-design-for-kids-vector.jpg',
+    type: 'birthday',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+  {
+    imgUrl:
+      'https://png.pngtree.com/element_our/20190531/ourlarge/pngtree-gorgeous-black-vintage-letter-frame-free-image_1283359.jpg',
+    type: 'memorial',
+  },
+];
